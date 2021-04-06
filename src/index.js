cfe0d9b84437b0d8b59feea25594e21875e86575
hlang <hamishlangmusic@gmail.com>
2021-04-06 12:34:51 +1000

    test

--- src/index.js
+++ src/index.js
@@ -1,80 +1,3 @@
-import * as database from './database';
-// import audio from '/assets/birds.mp3';
-
-
-const buildMessageRow = (messageItem) => {
-  const newMessageRow = document.createElement('tr');
-
-  newMessageRow.setAttribute('id', messageItem.id);
-  newMessageRow.innerHTML = `
-    <td>${messageItem.message}</td>
-    <td>${messageItem.votes}</td>
-    <td>
-      <i class="material-icons upvote">thumb_up</i>
-      <i class="material-icons downvote">thumb_down</i>
-      <i class="material-icons delete">delete</i>
-    </td>
-  `;
-
-  // todo: consider using delegation for this instead
-  newMessageRow.querySelector('.upvote').addEventListener('click', async () => {
-    await database.messages.updateVotes(messageItem.id, 1);
-    renderList();
-  });
-  newMessageRow.querySelector('.downvote').addEventListener('click', async () => {
-    await database.messages.updateVotes(messageItem.id, -1);
-    renderList();
-  });
-  newMessageRow.querySelector('.delete').addEventListener('click', async () => {
-    await database.messages.delete(messageItem.id);
-    renderList();
-  });
-
-  return newMessageRow;
-};
-
-const renderList = async () => {
-  const listContainer = document.getElementById('message-container');
-
-  // get latest messages
-  const messages = await database.messages.getAll();
-  
-  // reset list container to empty
-  listContainer.innerHTML = '';
-
-  // remove children from list container
-  messages.forEach((messageItem) => {
-    const newMessageRow = buildMessageRow(messageItem);
-    listContainer.append(newMessageRow);
-  });
-};
-
-
-const onLoadHandler = async () => {
-  // initial render of list
-  await renderList();
-
-  document.getElementById('submit').addEventListener('click', async (evt) => {
-    evt.preventDefault();
-
-    const newMessageInput = document.getElementById('newmessage');
-
-    // lets wait for the new message to be created before we request to render the list
-    await database.messages.create(newMessageInput.value);
-
-    renderList();
-
-    newMessageInput.value = '';
-  });
-};
-
-// Wait for DOM load
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', onLoadHandler);
-} else {
-  onLoadHandler();
-}
-
 
 
 // CLICK BATH
@@ -209,8 +132,11 @@ const piano1 = new Tone.Sampler({
 
 })
 
+// let flute1 = require('../static/flute 1.mp3');
+
 const flute = new Tone.Sampler({
   urls: {
+    // C6: flute1,
     C6: "flute 1.mp3",
     C5: "flute 2.mp3",
     C4: "flute 3.mp3",
@@ -691,4 +617,86 @@ if ($(`#c`).is(':checked') && $(`#major`).is(':checked')) {
 
 
 
+
+
+
+
+
+
+import * as database from './database';
+// import audio from '/assets/birds.mp3';
+
+
+const buildMessageRow = (messageItem) => {
+  const newMessageRow = document.createElement('tr');
+
+  newMessageRow.setAttribute('id', messageItem.id);
+  newMessageRow.innerHTML = `
+    <td>${messageItem.message}</td>
+    <td>${messageItem.votes}</td>
+    <td>
+      <i class="material-icons upvote">thumb_up</i>
+      <i class="material-icons downvote">thumb_down</i>
+      <i class="material-icons delete">delete</i>
+    </td>
+  `;
+
+  // todo: consider using delegation for this instead
+  newMessageRow.querySelector('.upvote').addEventListener('click', async () => {
+    await database.messages.updateVotes(messageItem.id, 1);
+    renderList();
+  });
+  newMessageRow.querySelector('.downvote').addEventListener('click', async () => {
+    await database.messages.updateVotes(messageItem.id, -1);
+    renderList();
+  });
+  newMessageRow.querySelector('.delete').addEventListener('click', async () => {
+    await database.messages.delete(messageItem.id);
+    renderList();
+  });
+
+  return newMessageRow;
+};
+
+const renderList = async () => {
+  const listContainer = document.getElementById('message-container');
+
+  // get latest messages
+  const messages = await database.messages.getAll();
+  
+  // reset list container to empty
+  listContainer.innerHTML = '';
+
+  // remove children from list container
+  messages.forEach((messageItem) => {
+    const newMessageRow = buildMessageRow(messageItem);
+    listContainer.append(newMessageRow);
+  });
+};
+
+
+const onLoadHandler = async () => {
+  // initial render of list
+  await renderList();
+
+  document.getElementById('submit').addEventListener('click', async (evt) => {
+    evt.preventDefault();
+
+    const newMessageInput = document.getElementById('newmessage');
+
+    // lets wait for the new message to be created before we request to render the list
+    await database.messages.create(newMessageInput.value);
+
+    renderList();
+
+    newMessageInput.value = '';
+  });
+};
+
+// Wait for DOM load
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', onLoadHandler);
+} else {
+  onLoadHandler();
+}
 
